e2344a6c67d6b6a4ab800e65036ed9a3b0166fb0
Nanis-9 <juanitaduranb09@gmail.com>
2020-07-27 16:21:19 -0500

    Primer cambio

---
+++ carpeta sin título/juego.js
@@ -0,0 +1,9 @@
+function CamiloLeal() {
+    var nombre = prompt(" ¿Cuál es el nombre y apellido del compañer@?");
+    nombre = nombre.toLowerCase();
+    if (nombre == "camilo leal") {
+        alert("Es correcto tienes 5 puntos");
+    } else {
+        alert("Es incorrecto tienes -2 puntos");
+    }
+}
---
+++ js/juego.js
@@ -0,0 +1,219 @@
+function CamiloLeal() {
+    var nombre = prompt(" ¿Cuál es el nombre y apellido del compañer@?");
+    nombre = nombre.toLowerCase();
+    if (nombre == "camilo leal") {
+        alert("Es correcto tienes 5 puntos");
+    } else {
+        alert("Es incorrecto tienes -2 puntos");
+    }
+}
+function MairaBarbosa() {
+    var nombre = prompt(" ¿Cuál es el nombre y apellido del compañer@?");
+    nombre = nombre.toLowerCase();
+    if (nombre == "maira barbosa") {
+        alert("Es correcto tienes 5 puntos");
+    } else {
+        alert("Es incorrecto tienes -2 puntos");
+    }
+}
+function Ansalo() {
+    var nombre = prompt(" ¿Cuál es el nombre y apellido del compañer@?");
+    nombre = nombre.toLowerCase();
+    if (nombre == "ansalo") {
+        alert("Es correcto tienes 5 puntos");
+    } else {
+        alert("Es incorrecto tienes -2 puntos");
+    }
+}
+function JuanVelasquez() {
+    var nombre = prompt(" ¿Cuál es el nombre y apellido del compañer@?");
+    nombre = nombre.toLowerCase();
+    if (nombre == "juan velasquez") {
+        alert("Es correcto tienes 5 puntos");
+    } else {
+        alert("Es incorrecto tienes -2 puntos");
+    }
+}
+function JulianMarciales() {
+    var nombre = prompt(" ¿Cuál es el nombre y apellido del compañer@?");
+    nombre = nombre.toLowerCase();
+    if (nombre == "julian marciales") {
+        alert("Es correcto tienes 5 puntos");
+    } else {
+        alert("Es incorrecto tienes -2 puntos");
+    }
+}
+function LauraCortes() {
+    var nombre = prompt(" ¿Cuál es el nombre y apellido del compañer@?");
+    nombre = nombre.toLowerCase();
+    if (nombre == "laura cortes") {
+        alert("Es correcto tienes 5 puntos");
+    } else {
+        alert("Es incorrecto tienes -2 puntos");
+    }
+}
+function BrayamBustos() {
+    var nombre = prompt(" ¿Cuál es el nombre y apellido del compañer@?");
+    nombre = nombre.toLowerCase();
+    if (nombre == "brayam bustos") {
+        alert("Es correcto tienes 5 puntos");
+    } else {
+        alert("Es incorrecto tienes -2 puntos");
+    }
+}
+function KarenVargas() {
+    var nombre = prompt(" ¿Cuál es el nombre y apellido del compañer@?");
+    nombre = nombre.toLowerCase();
+    if (nombre == "karen vargas") {
+        alert("Es correcto tienes 5 puntos");
+    } else {
+        alert("Es incorrecto tienes -2 puntos");
+    }
+}
+function CarlosGongora() {
+    var nombre = prompt(" ¿Cuál es el nombre y apellido del compañer@?");
+    nombre = nombre.toLowerCase();
+    if (nombre == "carlos gongora") {
+        alert("Es correcto tienes 5 puntos");
+    } else {
+        alert("Es incorrecto tienes -2 puntos");
+    }
+}
+function MariettaBeltran() {
+    var nombre = prompt(" ¿Cuál es el nombre y apellido del compañer@?");
+    nombre = nombre.toLowerCase();
+    if (nombre == "marietta beltran") {
+        alert("Es correcto tienes 5 puntos");
+    } else {
+        alert("Es incorrecto tienes -2 puntos");
+    }
+}
+function DanielVargas() {
+    var nombre = prompt(" ¿Cuál es el nombre y apellido del compañer@?");
+    nombre = nombre.toLowerCase();
+    if (nombre == "daniel vargas") {
+        alert("Es correcto tienes 5 puntos");
+    } else {
+        alert("Es incorrecto tienes -2 puntos");
+    }
+}
+function GabrielaBustos() {
+    var nombre = prompt(" ¿Cuál es el nombre y apellido del compañer@?");
+    nombre = nombre.toLowerCase();
+    if (nombre == "gabriela bustos") {
+        alert("Es correcto tienes 5 puntos");
+    } else {
+        alert("Es incorrecto tienes -2 puntos");
+    }
+}
+function AngieCartagena() {
+    var nombre = prompt(" ¿Cuál es el nombre y apellido del compañer@?");
+    nombre = nombre.toLowerCase();
+    if (nombre == "angie cartagena") {
+        alert("Es correcto tienes 5 puntos");
+    } else {
+        alert("Es incorrecto tienes -2 puntos");
+    }
+}
+function MiguelDias() {
+    var nombre = prompt(" ¿Cuál es el nombre y apellido del compañer@?");
+    nombre = nombre.toLowerCase();
+    if (nombre == "miguel dias") {
+        alert("Es correcto tienes 5 puntos");
+    } else {
+        alert("Es incorrecto tienes -2 puntos");
+    }
+}
+function JuanitaPeralta() {
+    var nombre = prompt(" ¿Cuál es el nombre y apellido del compañer@?");
+    nombre = nombre.toLowerCase();
+    if (nombre == "juanita peralta") {
+        alert("Es correcto tienes 5 puntos");
+    } else {
+        alert("Es incorrecto tienes -2 puntos");
+    }
+}
+function FelipeMartinez() {
+    var nombre = prompt(" ¿Cuál es el nombre y apellido del compañer@?");
+    nombre = nombre.toLowerCase();
+    if (nombre == "felipe martinez") {
+        alert("Es correcto tienes 5 puntos");
+    } else {
+        alert("Es incorrecto tienes -2 puntos");
+    }
+}
+function SantiagoOrtiz() {
+    var nombre = prompt(" ¿Cuál es el nombre y apellido del compañer@?");
+    nombre = nombre.toLowerCase();
+    if (nombre == "santiago ortiz") {
+        alert("Es correcto tienes 5 puntos");
+    } else {
+        alert("Es incorrecto tienes -2 puntos");
+    }
+}
+function CarlosReyes() {
+    var nombre = prompt(" ¿Cuál es el nombre y apellido del compañer@?");
+    nombre = nombre.toLowerCase();
+    if (nombre == "carlos reyes") {
+        alert("Es correcto tienes 5 puntos");
+    } else {
+        alert("Es incorrecto tienes -2 puntos");
+    }
+}
+function DanaCastillo() {
+    var nombre = prompt(" ¿Cuál es el nombre y apellido del compañer@?");
+    nombre = nombre.toLowerCase();
+    if (nombre == "dana castillo") {
+        alert("Es correcto tienes 5 puntos");
+    } else {
+        alert("Es incorrecto tienes -2 puntos");
+    }
+}
+function IvanVillamizar() {
+    var nombre = prompt(" ¿Cuál es el nombre y apellido del compañer@?");
+    nombre = nombre.toLowerCase();
+    if (nombre == "ivan villamizar") {
+        alert("Es correcto tienes 5 puntos");
+    } else {
+        alert("Es incorrecto tienes -2 puntos");
+    }
+}
+function DiegoPeres() {
+    var nombre = prompt(" ¿Cuál es el nombre y apellido del compañer@?");
+    nombre = nombre.toLowerCase();
+    if (nombre == "diego peres") {
+        alert("Es correcto tienes 5 puntos");
+    } else {
+        alert("Es incorrecto tienes -2 puntos");
+    }
+}
+function JorgeGomes() {
+    var nombre = prompt(" ¿Cuál es el nombre y apellido del compañer@?");
+    nombre = nombre.toLowerCase();
+    if (nombre == "jorge gomes") {
+        alert("Es correcto tienes 5 puntos");
+    } else {
+        alert("Es incorrecto tienes -2 puntos");
+    }
+}
+function LuzPulido() {
+    var nombre = prompt(" ¿Cuál es el nombre y apellido del compañer@?");
+    nombre = nombre.toLowerCase();
+    if (nombre == "luz pulido") {
+        alert("Es correcto tienes 5 puntos");
+    } else {
+        alert("Es incorrecto tienes -2 puntos");
+    }
+}
+function AlvaroMedina() {
+    var nombre = prompt(" ¿Cuál es el nombre y apellido del compañer@?");
+    nombre = nombre.toLowerCase();
+    if (nombre == "alvaro medina") {
+        alert("Es correcto tienes 5 puntos");
+    } else {
+        alert("Es incorrecto tienes -2 puntos");
+    }
+}
+
+
+
